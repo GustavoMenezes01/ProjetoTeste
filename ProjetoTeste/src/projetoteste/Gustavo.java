8296ee090ac28e0191ae3392ff2d36f44bde7701
819166518 <819166518@STNLI0112.animaedu.intranet>
2019-10-25 20:31:52 -0300

    versão Inicial

---
+++ ProjetoTeste/src/projetoteste/Gustavo.java
@@ -0,0 +1,11 @@
+package projetoteste;
+
+public class Gustavo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		System.out.println("Eu sou Gustavo");
+	}
+
+}
